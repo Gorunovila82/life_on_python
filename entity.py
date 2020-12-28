763aaadbfd08cc928866c1414276786364e15893
Gorunovila82 <64749284+Gorunovila82@users.noreply.github.com>
2020-12-28 11:40:54 +0300

    Update entity.py

--- entity.py
+++ entity.py
@@ -1,1 +1,21 @@
+import random
+
+
 class entity():
+    speed = 1
+    eat = 1
+    def __init__(self,parrent=None):
+        if parrent != None:
+            if random.randint(1,10) <= 2:
+                self.speed = parrent.speed + (random.random() / 2)
+                self.eat = parrent.eat + (random.random() / 2)
+            else:
+                self.speed = parrent.speed
+                self.eat = parrent.eat
+
+
+        elif random.randint(1,10) <= 2:
+                self.speed = self.speed + (random.random() / 2)
+                self.eat = self.eat + (random.random() / 2)
+
+
