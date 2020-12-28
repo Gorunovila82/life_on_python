6a372addee60ac4fb19bd5139824c64079fe77e4
Gorunovila82 <64749284+Gorunovila82@users.noreply.github.com>
2020-12-28 12:01:47 +0300

    Update entity.py
    Метод update

--- entity.py
+++ entity.py
@@ -4,6 +4,8 @@ import random
 class entity():
     speed = 1
     eat = 1
+    eattmp = 0
+    iskilled = False 
     def __init__(self,parrent=None):
         if parrent != None:
             if random.randint(1,10) <= 2:
@@ -17,5 +19,23 @@ class entity():
         elif random.randint(1,10) <= 2:
                 self.speed = self.speed + (random.random() / 2)
                 self.eat = self.eat + (random.random() / 2)
+
+    def update(self,disteat,eat):
+        eattmp = 0 
+        eattmp = (self.eat * eat.count)
+        print(eattmp)
+        if eattmp >= 1 and eattmp < 2:
+            return 1
+
+        elif eattmp >= 2:
+            return entity(self)
+        
+        else:
+            self.iskilled = True
+            return 0
+
+
+
+
 
 
